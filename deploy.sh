0d8df7021f505d68f4967ebb9c86fc509a88a43c
Sharang Phadke <sharang.phadke@gmail.com>
2022-08-30 16:22:49 -0400

    deploy update and setup info

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,3 @@ rm ../notion_emailer.zip
 zip -r ../notion_emailer.zip *
 cd ..
 aws lambda update-function-code --function-name WeeklyNotionEmail --zip-file fileb://notion_emailer.zip
-echo "deploy uploaded code from the lambda console"
